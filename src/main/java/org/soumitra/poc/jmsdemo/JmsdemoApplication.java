404126e97e5abadf73eaea55a6090b7953ff1f74
soumitrabanerjee <sama_664@yahoo.co.in>
2023-04-01 11:22:12 -0400

    Initial commit with jms volume

---
+++ src/main/java/org/soumitra/poc/jmsdemo/JmsdemoApplication.java
@@ -0,0 +1,13 @@
+package org.soumitra.poc.jmsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmsdemoApplication.class, args);
+	}
+
+}
