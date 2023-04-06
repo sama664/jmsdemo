652994e07ab0a22677646e7ae04e6f270bd91791
soumitrabanerjee <sama_664@yahoo.co.in>
2023-04-06 17:04:11 -0400

    post method to insert messages

--- src/main/java/org/soumitra/poc/jmsdemo/controller/SendmessageController.java
+++ src/main/java/org/soumitra/poc/jmsdemo/controller/SendmessageController.java
@@ -3,8 +3,8 @@ package org.soumitra.poc.jmsdemo.controller;
 import org.soumitra.poc.jmsdemo.model.Prodcut;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.jms.core.MessageCreator;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.LongAccumulator;
 
@@ -21,5 +21,10 @@ public class SendmessageController {
         jmsTemplate.convertAndSend("mailbox", new Prodcut("myproduct", "1234"));
 
         return result;
+    }
+    @PostMapping("/sendmessage")
+    public String SendMessage(@RequestBody Prodcut myproduct){
+        jmsTemplate.convertAndSend("mailbox",myproduct);
+        return "published through post";
     }
 }
